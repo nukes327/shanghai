4a8782b1462fc01d2667f08631dade861d1302dd
nukes327 <jake.nukes327@gmail.com>
2015-04-13 06:17:34 -0700

    Minor modifications

--- shanghaiv2.py
+++ shanghaiv2.py
@@ -1,4 +1,5 @@
 import socket
+import json
 
 class Bot:
     def __init__(self):
@@ -8,6 +9,7 @@ class Bot:
         self.nick = str.encode(f.readline().rstrip('\n'))
         self.key  = str.encode(f.readline().rstrip('\n'))
         self.users = {}
+        self.chanlist = []
         f.close()
 
     def connect(self):
@@ -19,17 +21,36 @@ class Bot:
 
     def join(self, channel):
         self.irc.send(b"JOIN " + channel + b"\r\n")
-        self.say(b"Connected to channel", channel)
+        print("Connected to channel " + bytes.decode(channel))
+        self.loadlist(channel)
 
     def say(self, msg, channel):
         self.irc.send(b"PRIVMSG " + channel + b" :" + msg + b"\r\n")
 
-    def quit(self):
+    def part(self, channel):
+        self.irc.send(b"PART " + channel + b"\r\n")
+        print("Writing userlist to file...")
+        f = open(channel, "w")
+        json.dump(self.users, f)
+        f.close()
         self.irc.send(b"QUIT\r\n")
         exit()
 
-    def loadlist(self, userlist):
-        pass
+    def loadlist(self, channel):
+        print("Loading user list for " + bytes.decode(channel))
+        try:
+            f = open(channel, "r")
+        except:
+            f = open(channel, "a+")
+            f.seek(0)
+            print("File not found, creating...")
+        try:
+            self.users = json.load(f)
+        except:
+            print("No user list found, initializing...")
+        else:
+            print("User list loaded, ready to go")
+        f.close()
 
     def listen(self):
         data = self.irc.recv(4096)
@@ -49,10 +70,13 @@ class Bot:
         user = data[1][0].split(b"!")[0]
         channel = data[1][2]
         color = data[0][0].split(b"=")[1]
+        if bytes.decode(user) not in self.users:
+            self.users[bytes.decode(user)] = {}
+            print("User " + bytes.decode(user) + " added to userlist")
         if msg.startswith(b"!"):
             msg = msg.lstrip(b"!")
-            if msg.startswith(b"quit"):
-                self.quit()
+            if msg.startswith(b"part"):
+                self.part(channel)
             if msg.startswith(b"echo"):
                 self.say(msg.split(b" ",maxsplit=1)[1], channel)
         elif msg.lower().startswith(b"shanghai"):
@@ -64,5 +88,6 @@ class Bot:
 shanghai = Bot()
 shanghai.connect()
 shanghai.join(b"#nukes327")
+shanghai.join(b"#soulrider95")
 while True:
     shanghai.listen()
