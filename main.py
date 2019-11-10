7c905292d9d667f2a96084813d204c0d96b723eb
nukes327 <jake.nukes327@gmail.com>
2019-11-09 21:52:47 -0500

    Adjusted logging functionality and initialization
    Logging now additionally logs messages of "WARNING" level and upwards
    in a separate errors file by default
    
    The main file was restructured to cut down on each function's complexity
    Functions were also renamed to better indicate their usage.
    
    If the logfile exists but is read-only the system will fail out with no
    recovery. I have yet to determine an appropriate way to handle this
    situation so this will remain for the time being.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 """What's actually run to use the bot
 
 Notes:
@@ -12,15 +14,108 @@ import configparser
 import logging
 import logging.config
 import os
-from pathlib import Path
 import sys
-from time import sleep
 
 import shanghai.shanghai as shanghai
 from shanghai.exceptions import ShanghaiError
 
 
-def init_shanghai():
+def init_logging() -> None:
+    """Check and recovery process for logging preparation"""
+    while True:
+        try:
+            logging.config.fileConfig('config/logging.ini')
+        except KeyError as inst:
+            logging_config_recovery(inst)
+        except FileNotFoundError as inst:
+            logging_logfile_recovery(inst)
+        except PermissionError as inst:
+            raise ShanghaiError(error=f'No access to default log file - {inst}')
+        except Exception as inst:
+            raise ShanghaiError(error=f'Unexpected exception, contact maintainer - {inst}')
+        else:
+            break
+
+
+def logging_config_recovery(issue: KeyError) -> None:
+    """Recovery checks and actions for accessing the logging configuration"""
+    print(f'There was an error reading the logging config: {issue}',
+          'Proceding with recovery',
+          sep='\n', file=sys.stderr)
+    try:
+        os.replace('config/logging.ini', 'config/logging.ini.old')
+    except FileNotFoundError:
+        try:
+            os.mkdir('config')
+        except FileExistsError:
+            pass
+        else:
+            print('Config directory was absent, created and continuing',
+                  file=sys.stderr)
+    else:
+        print('Renamed old file to logging.ini.old',
+              file=sys.stderr)
+    finally:
+        print('Creating new logging.ini',
+              file=sys.stderr)
+        create_logging_config()
+
+
+def logging_logfile_recovery(issue: FileNotFoundError) -> None:
+    """Recovery checks and actions for accessing the logfile"""
+    print(f'There was an error locating the log file or directory: {issue}',
+          file=sys.stderr)
+    try:
+        os.mkdir('logs')
+    except FileExistsError as inst:
+        raise ShanghaiError(error=f'Unexpected exception, contact maintainer - {inst}')
+    else:
+        print('Log directory was absent, created and continuing',
+              file=sys.stderr)
+
+
+def create_logging_config() -> None:
+    """Generate a standard logging config file"""
+    config = configparser.ConfigParser()
+    config['loggers'] = {'keys': 'root'}
+    config['handlers'] = {'keys': 'systemFileHandler, systemStreamHandler, errorFileHandler'}
+    config['formatters'] = {'keys': 'systemFormatter'}
+    config['logger_root'] = {
+        'level': 'INFO',
+        'handlers': 'systemFileHandler, systemStreamHandler, errorFileHandler'}
+    config['handler_systemFileHandler'] = {
+        'class': 'logging.handlers.RotatingFileHandler',
+        'level': 'INFO',
+        'formatter': 'systemFormatter',
+        'args': "('logs/shanghai.log', 'a', 10 * 1024 * 1024, 10,)"}
+    config['handler_systemStreamHandler'] = {
+        'class': 'StreamHandler',
+        'level': 'WARNING',
+        'formatter': 'systemFormatter',
+        'args': '(sys.stderr,)'}
+    config['handler_errorFileHandler'] = {
+        'class': 'logging.handlers.RotatingFileHandler',
+        'level': 'WARNING',
+        'formatter': 'systemFormatter',
+        'args': "('logs/shanghai_errors.log', 'a', 10 * 1024 * 1024, 10,)"}
+    config['formatter_systemFormatter'] = {
+        'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        'datefmt': ''}
+    with open('config/logging.ini', 'w+') as conffile:
+        config.write(conffile)
+
+
+def create_api_config() -> None:
+    """Create an empty apis config with necessary sections and keys"""
+    config = configparser.ConfigParser()
+    config['pixiv'] = {
+        'username': '',
+        'password': ''}
+    with open('config/apis.ini', 'w+') as conffile:
+        config.write(conffile)
+
+
+def create_shanghai_config() -> None:
     """Create an empty shanghai config with necessary sections and keys"""
     config = configparser.ConfigParser()
     config['DEFAULT'] = {
@@ -35,41 +130,8 @@ def init_shanghai():
     with open('config/shanghai.ini', 'w+') as conffile:
         config.write(conffile)
 
-def init_apis():
-    """Create an empty apis config with necessary sections and keys"""
-    config = configparser.ConfigParser()
-    config['pixiv'] = {
-        'username': '',
-        'password': ''}
-    with open('config/apis.ini', 'w+') as conffile:
-        config.write(conffile)
 
-def init_logging():
-    """Generate a standard logging config file"""
-    config = configparser.ConfigParser()
-    config['loggers'] = {'keys': 'root'}
-    config['handlers'] = {'keys': 'systemFileHandler, systemStreamHandler'}
-    config['formatters'] = {'keys': 'systemFormatter'}
-    config['logger_root'] = {
-        'level': 'INFO',
-        'handlers': 'systemFileHandler, systemStreamHandler'}
-    config['handler_systemFileHandler'] = {
-        'class': 'logging.handlers.RotatingFileHandler',
-        'level': 'INFO',
-        'formatter': 'systemFormatter',
-        'args': "('logs/shanghai.log', 'a', 10 * 1024 * 1024, 10,)"}
-    config['handler_systemStreamHandler'] = {
-        'class': 'StreamHandler',
-        'level': 'WARNING',
-        'formatter': 'systemFormatter',
-        'args': '(sys.stderr,)'}
-    config['formatter_systemFormatter'] = {
-        'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        'datefmt': ''}
-    with open('config/logging.ini', 'w+') as conffile:
-        config.write(conffile)
-
-def main():
+def main() -> None:
     """Main loop/code for initializing and running the bot
 
     Notes:
@@ -82,60 +144,19 @@ def main():
         configs itself later
 
     """
-    while True:
-        try:
-            logging.config.fileConfig('config/logging.ini')
-        except KeyError as inst:
-            print(f'There was an error reading the logging config: {inst}',
-                  'Proceding with recovery',
-                  sep='\n', file=sys.stderr)
-            try:
-                os.replace('config/logging.ini', 'config/logging.ini.old')
-            except FileNotFoundError:
-                try:
-                    os.mkdir('config')
-                except FileExistsError:
-                    pass
-                else:
-                    print('Config directory was absent, created and continuing',
-                          file=sys.stderr)
-            else:
-                print('Renamed old file to logging.ini.old',
-                      file=sys.stderr)
-            finally:
-                print('Creating new logging.ini',
-                      file=sys.stderr)
-                init_logging()
-            continue
-        except FileNotFoundError as inst:
-            print(f'There was an error locating the log file or directory: {inst}',
-                  file=sys.stderr)
-            try:
-                os.mkdir('logs')
-            except FileExistsError as inst:
-                # TODO: Dump traceback to a file, this shouldn't be reached
-                # NOTE: Maybe this can be reached if the log file exists and is read-only or inaccessible? Test later
-                raise ShanghaiError(f'Something unexpected went wrong, please contact repository maintainer - {inst}')
-            else:
-                print('Log directory was absent, created and continuing',
-                      file=sys.stderr)
-            continue
-        except Exception as inst:
-            raise ShanghaiError(f'Something unexpected went wrong, please contact repository maintainer - {inst}')
-        else:
-            break
-
+    init_logging()
     logger = logging.getLogger(__name__)
     logger.info('Logging configuration loaded')
     if not os.path.isfile('config/apis.ini'):
-        logger.warning('Apis config file missing, generating an emtpy one and continuing')
-        init_apis()
+        logger.warning('Apis config file missing, generating an empty one and continuing')
+        create_api_config()
     if not os.path.isfile('config/shanghai.ini'):
         logger.error('Main shanghai config file missing, generating an empty one and stopping')
-        init_shanghai()
+        create_shanghai_config()
     else:
         logger.info('Necessary configuration files are present, continuing')
         doll = shanghai.Bot()
+
 
 if __name__ == '__main__':
     main()
